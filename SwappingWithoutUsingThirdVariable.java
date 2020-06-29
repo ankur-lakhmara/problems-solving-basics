d36b9ad4030bf1524ec5f07ed7560937cd067b6f
Ankur Rohilla <59440628+ankur-lakhmara@users.noreply.github.com>
2020-06-29 23:58:27 +0530

    this question Asked companies Interviews

--- SwappingWithoutUsingThirdVariable.java
+++ SwappingWithoutUsingThirdVariable.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class SwappingWithoutUsingThirdVariable {
     public static void main(String[]arg){
         System.out.println("Enter numbers for swapping");
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //in my case i use sc class for scanner you may change it and also change in Int 
         System.out.println("enter 1st number");
         int a = sc.nextInt();
         System.out.println("Enter 2nd number");
         int b = sc.nextInt();
-        a = a+b;
+        a = a+b; 
         b = a-b;
         a = a-b;
         {
