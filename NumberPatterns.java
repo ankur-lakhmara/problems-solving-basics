bef5c545db61cc29f24de4ed0898dd4142891fbd
Ankur Rohilla <59440628+ankur-lakhmara@users.noreply.github.com>
2020-10-19 12:10:28 +0530

    NumberPatterns.java

---
+++ NumberPatterns.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class NumberPatterns {
+    public static void main(String[]arg){
+        System.out.println("Enter int number");
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i = 1;i<=n;i++){
+            for(int j=1;j<=n+1-i;j++){
+                System.out.print(j+" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
